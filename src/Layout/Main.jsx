3dad165909db01837d9496bf581bc0872263a666
soharab007 <soharabhossain943@gmail.com>
2022-10-12 03:42:44 +0600

    site responsive

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Header from '../Components/Header';
 // import Header from '../Components/Header';
+// import Header from '../Components/Header';
 
 const Main = () => {
     return (
